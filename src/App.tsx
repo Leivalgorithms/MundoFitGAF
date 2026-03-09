8fb4ed7328facb0fa272610e91436d36e544cabd
mauledji <mauledji@outlook.com>
2026-03-09 00:37:42 -0600

    Setup inicial: Vite + React + TS + Tailwind v4 + dependencias Mundo Fit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold text-red-600 p-8">Mundo Fit</h1>
+    </div>
+  )
+}
+
+export default App
